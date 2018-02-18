5559157b99af69c1ecfeb4c14fb91a4d5ff7a85d
xingyuz233 <qq80325987@163.com>
2018-02-19 03:01:46 +0800

    2018-2-19
    des

---
+++ app/src/main/java/com/example/xyzhang/testapp/home_display.java
@@ -0,0 +1,13 @@
+package com.example.xyzhang.testapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class home_display extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_display);
+    }
+}
--- app/src/main/java/com/example/xyzhang/testapp/info.java
+++ app/src/main/java/com/example/xyzhang/testapp/info.java
@@ -2,9 +2,13 @@ package com.example.xyzhang.testapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class info extends AppCompatActivity {
-
+    private TextView tabHome;
+    private TextView tabUpload;
+    private TextView tabUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
