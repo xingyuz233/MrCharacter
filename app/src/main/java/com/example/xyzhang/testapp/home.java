2e822152656a90dc8728f63f1702f09c5c5158fb
xingyuz233 <qq80325987@163.com>
2018-02-19 20:01:31 +0800

    ...

--- app/src/main/java/com/example/xyzhang/testapp/home.java
+++ app/src/main/java/com/example/xyzhang/testapp/home.java
@@ -3,7 +3,7 @@ package com.example.xyzhang.testapp;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -58,6 +58,8 @@ public class home extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+
     }
 
     @Override
--- app/src/main/java/com/example/xyzhang/testapp/info.java
+++ app/src/main/java/com/example/xyzhang/testapp/info.java
@@ -1,17 +1,110 @@
 package com.example.xyzhang.testapp;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
-public class info extends AppCompatActivity {
+public class info extends AppCompatActivity implements View.OnClickListener{
     private TextView tabHome;
     private TextView tabUpload;
     private TextView tabUser;
+
+    private FrameLayout ly_content;
+
+    private home f1;
+    private upload f2;
+    private user f3;
+    private FragmentManager fragmentManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_info);
+
+        bindView();
+
+    }
+
+    //UI组件初始化与事件绑定
+    private void bindView() {
+        tabHome = (TextView)this.findViewById(R.id.txt_home);
+        tabUpload = (TextView)this.findViewById(R.id.txt_upload);
+        tabUser = (TextView)this.findViewById(R.id.txt_user);
+        ly_content = (FrameLayout) findViewById(R.id.fragment_container);
+
+        tabHome.setOnClickListener(this);
+        tabUser.setOnClickListener(this);
+        tabUpload.setOnClickListener(this);
+
+    }
+
+    //重置所有文本的选中状态
+    public void selected(){
+        tabHome.setSelected(true);
+        tabUpload.setSelected(false);
+        tabUser.setSelected(false);
+    }
+
+    //隐藏所有Fragment
+
+    public void hideAllFragment(FragmentTransaction transaction){
+        if(f1!=null){
+            transaction.hide(f1);
+        }
+        if(f2!=null){
+            transaction.hide(f2);
+        }
+        if(f3!=null){
+            transaction.hide(f3);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        hideAllFragment(transaction);
+        switch(v.getId()){
+            case R.id.txt_home:
+                selected();
+                tabHome.setSelected(true);
+                if(f1==null){
+                    f1 = new home();
+                    transaction.add(R.id.fragment_container,f1);
+                }else{
+                    transaction.show(f1);
+                }
+                break;
+
+            case R.id.txt_upload:
+                selected();
+                tabUpload.setSelected(true);
+                if(f2==null){
+                    f2 = new upload();
+                    transaction.add(R.id.fragment_container,f2);
+                }else{
+                    transaction.show(f2);
+                }
+                break;
+
+            case R.id.txt_user:
+                selected();
+                tabUser.setSelected(true);
+                if(f3==null){
+                    f3 = new user();
+                    transaction.add(R.id.fragment_container, f3);
+                }else{
+                    transaction.show(f3);
+                }
+                break;
+        }
+
+        transaction.commit();
     }
 }
